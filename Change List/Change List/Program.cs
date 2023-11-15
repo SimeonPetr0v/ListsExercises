d687a4744e9af037fd42ae32f33ccd90e2978f11
SimeonPetr0v <simeonp925@gmail.com>
2023-11-15 12:24:35 +0200

    Homework

---
+++ Change List/Change List/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Train/Train/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> train = GetIntList();
+            int capacity = int.Parse(Console.ReadLine());
+            string[] command = GetStringArray();
+            while (command[0]?.ToLower() != "end")
+            {
+                switch (command[0]?.ToLower())
+                {
+                    case "add":
+                        //int num = int.Parse(command[1]);
+                        //train.Add(num);
+                        train.Add(int.Parse(command[1]));
+                        break;
+                    default:
+                        AddNumberToList(train, capacity, command[0]);
+                        break;
+                }
+                command = GetStringArray();
+            }
+            Console.WriteLine(string.Join(" ", train));
+
+        }
+        static List<int> GetIntList()
+        {
+            return Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToList();
+        }
+        static string[] GetStringArray ()
+        {
+            return Console.ReadLine()
+                .Split(" ")
+                .ToArray();
+        }
+        static List<int> AddNumberToList(List <int> list, int capacity, string num)
+        {
+            int convertedNumber = int.Parse(num);
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (list[i] + convertedNumber <= capacity)
+                {
+                    list[i] += convertedNumber;
+                    break;
+                }
+            }
+            return list;
+        }
+    }
+}
